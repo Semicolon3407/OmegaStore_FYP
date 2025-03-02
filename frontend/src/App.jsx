073cfa5d1268f07078cad014d0347a7efca9fe41
AnupSharma3407 <np01cp4a220508@islingtoncollege.edu.np>
2025-03-02 18:25:36 +0545

    started frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+ <div>
+ <h1 className='text-4xl text-red-500'>Hello bhbsqjhbdhb</h1>
+ </div>
+    </>
+  )
+}
+
+export default App
